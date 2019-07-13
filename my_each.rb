c67c748d93767a5132b865776ab8ede32252c426
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 02:24:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if array.length > 0
+    i = 0
+    while array.length > i
+      yield array[i]
+      i += 1
+    end
+    array
+  end
 end
